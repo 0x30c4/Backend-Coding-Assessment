8f77f268ee7530bf698f1c15e63638de024c77f6
0x30c4 <abdullahsanaf@gmail.com>
2022-04-25 02:30:07 +0600

    Basic

---
+++ backend/run_local.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+export SECRET_KEY="django-insecure-a1ah*vm=+dmbg85)6v_*z!e97@49w!goe2f%msng6b-#2(ny@)"
+export DEBUG=1
+
+# DB CRED's
+export DB_NAME=fintech
+export USERNAME=root
+export PASSWORD=1234
+export HOST=192.168.0.100
+export PORT=1111
+
+export PROD_HOST_DOMAIN="biniyogfintech.0x30c4.dev"
+
+export STATIC_URL="static/"
+
+export DEBUG=0
+
+if [[ $# > 0 ]]
+then
+	./manage.py migrate
+else
+	./manage.py runserver
+fi
